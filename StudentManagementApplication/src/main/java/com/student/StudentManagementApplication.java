cabfa41e0054bb908e91fd08ad9d3d7ca461783d
unknown <cogjava4437@iiht.tech>
2022-11-17 14:28:45 +0530

    first spring boot project with hello word

---
+++ StudentManagementApplication/src/main/java/com/student/StudentManagementApplication.java
@@ -0,0 +1,24 @@
+package com.student;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class StudentManagementApplication {
+	
+	
+	@GetMapping("/helloworld")
+	public String helloword() {
+		return "hello  i am java Developer";
+		
+	}
+	
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentManagementApplication.class, args);
+	}
+
+}
